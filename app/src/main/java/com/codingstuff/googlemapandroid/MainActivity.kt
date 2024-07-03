94bea4c63b52acc0754203e1bb01af98b56d02c7
codingstuff070 <codingstuff070@gmail.com>
2024-07-03 21:25:59 +0530

    map types

--- app/src/main/java/com/codingstuff/googlemapandroid/MainActivity.kt
+++ app/src/main/java/com/codingstuff/googlemapandroid/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.codingstuff.googlemapandroid
 
 import android.os.Bundle
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,9 +10,13 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.UiSettings
 import com.google.android.gms.maps.model.LatLng
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var radioGroup: RadioGroup
+    private var map: GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +27,27 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             insets
         }
 
+        radioGroup = findViewById(R.id.radioGroup)
+        radioGroup.setOnCheckedChangeListener { _, itemId: Int ->
+            when (itemId) {
+                R.id.normalBtn -> {
+                    map?.mapType = GoogleMap.MAP_TYPE_NORMAL
+                }
+
+                R.id.satelliteBtn -> {
+                    map?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+                }
+
+                R.id.hybridBtn -> {
+                    map?.mapType = GoogleMap.MAP_TYPE_HYBRID
+                }
+
+                R.id.terrainBtn -> {
+                    map?.mapType = GoogleMap.MAP_TYPE_TERRAIN
+                }
+
+            }
+        }
         val mapFragment =
             supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
 
@@ -29,7 +55,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(map: GoogleMap?) {
+        this.map = map
+        map?.mapType = GoogleMap.MAP_TYPE_NORMAL
+        map?.uiSettings?.isZoomControlsEnabled = true
         val latLng = LatLng(28.7041, 77.1025)
-        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,19f))
+        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 19f))
     }
 }
